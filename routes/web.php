43354d595c5a65603fccccb8a5efb02d689b2fc9
Intan <nurintanqolbinuri23@politala.ac.id>
2024-10-23 12:18:14 +0800

    halaman

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,3 @@ use App\Http\Controllers\HomeController;
 // Route untuk halaman home
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Route untuk resource products
-Route::resource('products', \App\Http\Controllers\ProductController::class);
